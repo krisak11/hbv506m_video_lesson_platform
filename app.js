266fc5bd974b55a38c1466f61bb5434ab6058c0a
Lov??sa S??l <kik7@hi.is>
2026-02-14 16:15:33 +0000

    Add local env support for development

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
